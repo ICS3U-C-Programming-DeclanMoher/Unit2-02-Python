88bce395ef8b9edfb655cce98556209e4543bb86
Declan Moher <declan.moher@stu.ocsb.ca>
2023-09-28 15:31:08 +0000

    added a area and perimeter of a rectangle

---
+++ area_per_rect_input.py
@@ -0,0 +1,21 @@
+import math
+
+
+def main():
+    # input
+
+    l = 7
+    w = 9
+
+    # process
+    area = l * w
+    perimeter = 2 * (l + w)
+
+    # output
+    print("")
+    print("Area = {} cm".format(area))
+    print("Perimeter = {} cm".format(perimeter))
+
+
+if __name__ == "__main__":
+    main()
